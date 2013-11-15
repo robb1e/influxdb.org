53cef54669d59821fa674ae7fb050f27a2159d16
Todd Persen <todd.persen@gmail.com>
2013-11-15 16:36:56 -0500

    Add a basic blog layout.

--- config.rb
+++ config.rb
@@ -24,6 +24,10 @@ with_layout :docs do
   page "/docs/*"
 end
 
+with_layout :blog do
+  page "/blog/*"
+end
+
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
